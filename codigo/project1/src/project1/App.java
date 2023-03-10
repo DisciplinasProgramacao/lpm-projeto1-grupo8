67fc5c4f8f1a968498637b0d90c21127689a025b
Arthur Jansen <artjansentec@gmail.com>
2023-03-10 19:26:21 -0300

    criando exemplo de execução main #16

--- codigo/project1/src/project1/App.java
+++ codigo/project1/src/project1/App.java
@@ -1,6 +1,27 @@
 package project1;
 public class App {
-    public static void main(String[] args){
-         System.out.println("Roda roda");
+    public static void main(String[] args) throws Exception{
+        Produto produto;
+        Produto produto2;
+        Mercearia mercearia;
+
+        produto = new Produto("banana doce", 23, 9.50, 66);
+        produto2 = new Produto("maça azeda", 10, 10, 70);
+        mercearia = new Mercearia();
+
+        produto.efetuarVenda(5);
+        produto.efetuarCompra(6);
+
+        mercearia.adicionarProduto(produto.getDescricao(), 9.50, 66, produto.getQuantidadeEstoque());
+        mercearia.adicionarProduto(produto2.getDescricao(), 10, 70, produto2.getQuantidadeEstoque());
+        mercearia.receberQtdProdutosCadastradosEstoque();
+        mercearia.removerProduto(produto2);
+        mercearia.receberQtdProdutosCadastradosEstoque();
+        mercearia.receberNomesProdutosAbaixoMinimoEstoque();
+        mercearia.receberValorTotalEmEstoque();
+        mercearia.receberProdutoPorID(produto.getID());
+        mercearia.receberProdutoPorDescricao(produto.getDescricao());
+         
+
     }
 }
