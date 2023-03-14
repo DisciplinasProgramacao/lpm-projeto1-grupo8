590a5f6cae93d18bcc10eece9b0edb4037d0f34b
henriquegrissi <hgcsc2003@gmail.com>
2023-03-14 13:07:27 -0300

    Voltando o método alterarDescricao para o atributo

--- codigo/project1/src/project1/Produto.java
+++ codigo/project1/src/project1/Produto.java
@@ -59,7 +59,7 @@ public class Produto {
 	// #region CONSTRUTORES
 	private void init(String descricao, int quantidadeTotalAdquirida, double precoCusto, double margemLucro) {
 		this.ID = parseID++;
-		this.descricao = descricao; //alterarDescricao(descricao);
+		this.descricao = alterarDescricao(descricao);
 		this.quantidadeEstoque = 0;
 		this.quantidadeTotalComprada = 0;
 		this.quantidadeTotalVendas = 0;
